650817973a9d1428b11736f852cc2b9649f5ec10
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-02-05 22:17:40 +0100

    New model

--- pyimagesearch/smallervggnet.py
+++ pyimagesearch/smallervggnet.py
@@ -23,48 +23,55 @@ class SmallerVGGNet:
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
-        
-        # CONV => RELU => POOL
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(3, 3)))
-		model.add(Dropout(0.25))
 
-        # We’re increasing our filter size from 32  to 64 . The deeper we go in the network, the smaller the spatial dimensions of our volume, and the more filters we learn.
-        # We decreased how max pooling size from 3 x 3  to 2 x 2  to ensure we do not reduce our spatial dimensions too quickly.
+        # # CONV => RELU => POOL
+		# model.add(Conv2D(32, (3, 3), padding="same",
+		# 	input_shape=inputShape))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization(axis=chanDim))
+		# model.add(MaxPooling2D(pool_size=(3, 3)))
+		# model.add(Dropout(0.25))
 
-        # (CONV => RELU) * 2 => POOL
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+        # # We’re increasing our filter size from 32  to 64 . The deeper we go in the network, the smaller the spatial dimensions of our volume, and the more filters we learn.
+        # # We decreased how max pooling size from 3 x 3  to 2 x 2  to ensure we do not reduce our spatial dimensions too quickly.
 
-        # (CONV => RELU) * 2 => POOL
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+        # # (CONV => RELU) * 2 => POOL
+		# model.add(Conv2D(64, (3, 3), padding="same"))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization(axis=chanDim))
+		# model.add(Conv2D(64, (3, 3), padding="same"))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization(axis=chanDim))
+		# model.add(MaxPooling2D(pool_size=(2, 2)))
+		# model.add(Dropout(0.25))
 
-        # first (and only) set of FC => RELU layers
-		model.add(Flatten())
-		model.add(Dense(1024))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization())
-		model.add(Dropout(0.5))
+        # # (CONV => RELU) * 2 => POOL
+		# model.add(Conv2D(128, (3, 3), padding="same"))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization(axis=chanDim))
+		# model.add(Conv2D(128, (3, 3), padding="same"))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization(axis=chanDim))
+		# model.add(MaxPooling2D(pool_size=(2, 2)))
+		# model.add(Dropout(0.25))
 
-		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # # first (and only) set of FC => RELU layers
+		# model.add(Flatten())
+		# model.add(Dense(1024))
+		# model.add(Activation("relu"))
+		# model.add(BatchNormalization())
+		# model.add(Dropout(0.5))
+
+		# # softmax classifier
+		# model.add(Dense(classes))
+		# model.add(Activation("softmax"))
+
+        # Versión simplificada pero en principio peores resultados pero entrenamiento más rápido, quizás al poder
+		# tener más épocas en el mismo tiempo que el modelo de arriba pueda ser mejor ¿?
+		model.add(Flatten(input_shape=(96, 96, 3), name="Input_layer"))
+		model.add(Dense(100, activation='relu', name="Hidden_layer_1"))
+		model.add(Dense(50, activation='relu', name="Hidden_layer_2"))
+		model.add(Dense(5, activation='softmax', name="Output_layer"))
 
 		# return the constructed network architecture
 		return model
